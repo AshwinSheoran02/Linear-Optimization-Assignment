82eb862f290f975c9089cccb7ceea4199748670d
VIBHOR AGARWAL <vibhor20349@iiitd.ac.in>
2023-04-29 13:58:43 +0530

    Added Comments

--- Final/Q4a.py
+++ Final/Q4a.py
@@ -1,57 +1,75 @@
 import numpy as np
 import sympy
 
-eps = 1e-4
+eps = 1e-4  # tolerance for stopping criterion
 
-def prepareA(A,b,c):
+# function to prepare the input matrices for solving
+def prepareA(A, b, c):
     m, n = A.shape
     rankA = np.linalg.matrix_rank(A)
-    if(rankA != min(m, n)):
+    # if matrix A is not full rank, drop redundant rows using row-reduced echelon form
+    if rankA != min(m, n):
         _, rrefPivot = sympy.Matrix(A).T.rref()
         A = A[list(rrefPivot)]
 
     return A
 
+# function to solve the integral point linear program using interior point method
 def integralPointSolver(A, b, c):
     m, n = A.shape
-    modifiedA = prepareA(A, b,c)
-    if(modifiedA.all() == None):
-        return
+    modifiedA = prepareA(A, b, c)  # prepare the input matrices
+    if modifiedA.all() == None:
+        # if there is no solution, return None
+        return None
     A = modifiedA
 
+    # initialize the solution with all ones
     slackSol = np.ones(n)
     primalSol = np.ones(n)
     dualSol = np.ones(m)
 
+    # calculate the initial dot product of primalSol and slackSol
     xDotS = np.dot(primalSol, slackSol)
-    while(True):
-        muK = xDotS/n
+
+    # iterate until the dot product of primalSol and slackSol is less than the tolerance value eps
+    while True:
+        # calculate the current value of the barrier parameter
+        muK = xDotS / n
+
+        # set the centering parameter
         sigmaK = 0.4
 
-        Anew = np.zeros((m+2*n, m+2*n))
+        # construct the augmented matrix for solving the system of linear equations
+        Anew = np.zeros((m + 2*n, m + 2*n))
         Anew[0:m, 0:n] = np.copy(A)
         Anew[m:m+n, n:m+n] = np.copy(A.T)
         Anew[m:m+n, m+n:m+2*n] = np.eye(n)
         Anew[m+n:m+2*n, 0:n] = np.copy(np.diag(slackSol))
         Anew[m+n:m+2*n, n+m:m+2*n] = np.copy(np.diag(primalSol))
 
-        bnew = np.zeros(m+2*n)
-        bnew[0:m] = np.copy(b-np.dot(A, primalSol))
-        bnew[m:m+n] = np.copy(c-np.dot(A.T, dualSol)-slackSol)
-        bnew[m+n:m+2*n] = np.copy(muK*sigmaK*np.ones(n)-np.dot(np.dot(np.diag(slackSol), np.diag(primalSol)), np.ones(n)))
+        # construct the right-hand side of the linear system
+        bnew = np.zeros(m + 2*n)
+        bnew[0:m] = np.copy(b - np.dot(A, primalSol))
+        bnew[m:m+n] = np.copy(c - np.dot(A.T, dualSol) - slackSol)
+        bnew[m+n:m+2*n] = np.copy(muK * sigmaK * np.ones(n) - np.dot(np.dot(np.diag(slackSol), np.diag(primalSol)), np.ones(n)))
 
+        # solve the linear system to obtain the primal, dual, and slack variables
         delta = np.linalg.solve(Anew, bnew)
         ds = delta[n+m:m+2*n]
         dx = delta[0:n]
         dl = delta[n:n+m]
 
+        # calculate the maximum step length for primal and slack variables
         alphaMax = min(np.where(ds < 0, -slackSol/ds, np.inf).min(), np.where(dx < 0, -primalSol/dx, np.inf).min())
 
-
+        # set the step length reduction factor
         etaK = 0.98
-        alphaK = min(etaK*alphaMax, 1)
 
-        slackSol = slackSol+alphaK*ds
+        # calculate the step length
+        alphaK = min(etaK * alphaMax, 1)
+
+        # update the primal, dual, and slack variables using the step lengths
+        slackSol = slackSol + alphaK * ds
         primalSol = primalSol+alphaK*dx
         dualSol = dualSol+alphaK*dl
 
